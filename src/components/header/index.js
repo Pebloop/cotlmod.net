bff9b5c4ea2ad363cd39b1ba55440aa3b0f54830
fenndev <fenndev@protonmail.com>
2022-09-04 23:56:25 -0700

    Fixed rel="noreferrer" vulnerability

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,10 +7,10 @@ const Header = () => (
 		<h1>Cult of the Lamb Modsite</h1>
 		<nav>
 			<Link activeClassName={style.active} href="/">Home</Link>
-			<a href="https://cult-of-the-lamb.thunderstore.io/" target="_blank">Mods</a>
+			<a href="https://cult-of-the-lamb.thunderstore.io/" target="_blank" rel="noreferrer">Mods</a>
 			<Link activeClassName={style.active} href="/profile/john">Tutorials</Link>
 			<Link activeClassName={style.active} href="/profile/bruh">Tools</Link>
-			<a href="https://discord.gg/MUjww9ndx2" target="_blank">Discord</a>
+			<a href="https://discord.gg/MUjww9ndx2" target="_blank" rel="noreferrer">Discord</a>
 			
 		</nav>
 	</header>
